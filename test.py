10a29c91baa274b8fb59dd56308d9496cccee581
YuYing_Liang <yuying.liang00@gmail.com>
2019-09-08 00:32:23 -0400

    added terms and conditions

--- test.py
+++ test.py
@@ -41,7 +41,7 @@ class Handler:
                 "`!quizme` to quiz yourself about security and privacy \n " + 
                 "`!ask <question>` to ask a question about security \n " +
                 "`!findme <google or reddit>` to learn more about your public profile!" + 
-                "`!termsAndService <company>` to learn more about the conditions and privacy policies " + 
+                "`!evaluate <company>` to learn more about the conditions and privacy policies " + 
                                 "of many popular internet companies :slightly_smiling_face: ")
 
             #meme function
@@ -164,6 +164,50 @@ class Handler:
                     await bot.chat.send(channel, "Sorry, we didn't find anything about you :confused:")
                 self.findMe = False
 
+            elif "!evaluate" in msg:
+                terms_list = []
+                goodbad_list = []
+
+                keyword = msg.split(" ")
+                keyword.remove("!evaluate")
+                companies = ["amazon", "duckduckgo", "github", "youtube"]
+                companies_json = ["json/amazon.json", "json/duckduckgo.json", "json/github.json", "json/youtube.json"]
+                for i in range(4):
+                    if keyword[0] == companies[i]:
+                        company_json = companies_json[i]
+                with open(company_json, 'r') as json_file:
+                    data = json_file.read()
+                    tosdr = json.loads(data)
+                    points = tosdr["points"]
+                    for point in points:
+                        terms_list += [tosdr["pointsData"][point]["title"]]
+                        goodbad_list += [tosdr["pointsData"][point]["tosdr"]["point"]]
+
+                emoji_list = [None] * len(goodbad_list)
+                for x in range(len(goodbad_list)):
+                    if goodbad_list[x] == "good":
+                        emoji_list[x] = ':+1:'#'/U0001F44D'
+                    elif goodbad_list[x] == "bad":
+                        emoji_list[x] = ':-1:'#'/U0001F44E'
+                    elif goodbad_list[x] == "neutral":
+                        emoji_list[x] = ':neutral_face:'#'/U0001F636'
+
+                goodctr = 0
+                neutralctr = 0
+                badctr = 0
+
+                for y in range(len(goodbad_list)):
+                    if goodbad_list[y] == "good":
+                        goodctr += 1
+                    elif goodbad_list[y] == "bad":
+                        badctr += 1
+                    elif goodbad_list[y] == "neutral":
+                        neutralctr += 1
+
+                for j in range(len(terms_list)):
+                    await bot.chat.send(channel, str((j + 1)) + ". " + emoji_list[j]+ " " + terms_list[j])
+
+                await bot.chat.send(channel, ':+1:' + ": " + str(goodctr) + ", " + ':neutral_face:' + ": " + str(neutralctr) + ", " + ':-1:' + ": " + str(badctr))
 
 listen_options = {
     "local": True,
